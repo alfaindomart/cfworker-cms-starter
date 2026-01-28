c8efb2e5c2f3fa8fc6768a65f883243b74bb4a97
alfaindomart <alfaindomart23@gmail.com>
2026-01-28 19:35:11 +0700

    setup better auth

--- src/routes/collections.ts
+++ src/routes/collections.ts
@@ -2,11 +2,22 @@ import { Hono } from "hono";
 import * as schemaValidator from "../dtos/index"
 import z from "zod";
 import {zValidator} from "@hono/zod-validator";
+import { dbProvider } from "../middleware/dbProvider";
+import * as schema from '../db/schema'
 
 const app = new Hono()
 
-.get("/all", )
+.use("*", dbProvider)
 
+.get("/all", async (c) => {
+    console.log('getting database')
+    const db = c.var.db
+    const query = await db.select().from(schema.collections)
+    return c.json({
+        success: true,
+        data: query
+    })
+})
 .post("/new", zValidator("json", schemaValidator.ZCollectionInsert), async (c) => {
     const body = c.req.valid("json")
     return c.json({
@@ -14,3 +25,5 @@ const app = new Hono()
         data: body
     })
 })
+
+export default app
